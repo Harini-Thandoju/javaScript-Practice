8ab9622643f5c7839c9e8969d1911ae09032144d
Harini-Thandoju <harinithandoju@gmail.com>
2021-09-06 01:06:37 +0530

    if else statements

--- app.js
+++ app.js
@@ -1,66 +1,19 @@
-//Arithmetic operators
-let age=19;
-age=20;/*value or type anything can b changed if we ue let */
-console.log(age);
-const specific_name="Harini";/*it is fixed we cant reassign*/
-console.log(specific_name);
+//conditional statements
+//if else
+//check whether number is eve or odd
+const a=0;
+if(typeof a=='number'){
+   if(a==0){
+    console.log("Neither even nor odd")
+}
+else if(a%2==0){
+    console.log("this is a even number")
+}
+else{
+    console.log("This is a odd number")
+}
 
-const a=2;
-const b=5;
-
-console.log(a+b);
-console.log(a-b);
-console.log(a*b);
-console.log(a/b);
-console.log(a**b)//2 pow 5(exponents)
-console.log(a%b);
-console.log('___________________________________________________________');
-let c=10;
-c++
-//c+=1  or c=c+1
-c+=5;
-c**=3;
-console.log(c)
-const d="one";
-const e="two";
-console.log(d+e);
-const f='2'
-console.log(a+f)
-console.log(a-f)
-//Assignment operators
-let Name="Harini";
-let o=10;
-o+=Name;
-console.log(typeof o)
-//comparision operator
-const j=3;
-const k=9;
-const l=3;
-console.log('j>k',j>k);
-console.log('j>k',j<k);
-console.log('j>k',j==k);
-console.log('j>k',j==k);
-console.log('j>k',j>=k)
-console.log('___________________________________________________________');
-const m=11;
-const n='11';
-console.log(m==n)//it doesnt compare the types just compares the value
-console.log(m===n)
-//Logical operators
-const x=2;
-const y=3;
-const z=5;
-console.log('x==y',x==y)
-console.log('x!=y',x!=y)
-console.log('Truth table for AND')
-console.log('false && false',false && false)
-console.log('false && true',false && true)
-console.log('true && false',true && false)
-console.log('true && true',true && true)
-console.log(x>z && y>z)
-console.log('Truth table for OR')
-console.log('false || false',false || false)
-console.log('false || true',false || true)
-console.log('true || false',true || false)
-console.log('true || true',true ||true)
-console.log(x>z || y>z)
+}
+else{
+    console.error("not a number")
+}

db4227cac76aaf7a01306c4d2d51d4ca3da49df1
Harini-Thandoju <harinithandoju@gmail.com>
2021-09-05 22:56:19 +0530

    data types

--- app.js
+++ app.js
@@ -6,3 +6,22 @@ console.log(false)
 console.log(false,234567,'string',{name:"Harini"})
 console.warn("This is warning")
 console.error("This is an error")
+//comment line......if no.of line ctrl+/
+/*multi line comment*/
+//data types
+//primitive data types
+let age=19.87654;
+console.log(age);
+console.log(typeof age);
+let Name='kmjnhbgf!@#$32345';
+console.log(typeof Name);
+var name ="harini";
+console.log(name);
+let brand=null;
+console.log(brand)
+//non-primitive datatype
+let array=[1,2,3,4,5];
+array.push("value")
+array.push(true)
+console.log(array)
+console.log(typeof array)

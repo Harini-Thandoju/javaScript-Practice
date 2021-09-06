ffb55318b1b5eff4644f5f8126ba716a841827dc
Harini-Thandoju <harinithandoju@gmail.com>
2021-09-06 17:24:57 +0530

    node method

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
-//conditional statements
-//if else
-//check whether number is eve or odd
-const a=0;
-if(typeof a=='number'){
-   if(a==0){
-    console.log("Neither even nor odd")
-}
-else if(a%2==0){
-    console.log("this is a even number")
-}
-else{
-    console.log("This is a odd number")
-}
-
-}
-else{
-    console.error("not a number")
-}
+const element= document.getElementById('select_me')
+console.log(element);
+console.log(typeof element);
+const element1= document.getElementsByClassName('heading')
+console.log(element1);
+const element2=document.getElementsByTagName('div')
+console.log(element2);
+const element3=document.querySelector('div#select_me');
+console.log(element3);
+element.innerHTML="This is from JS"//we can change the content of the element
+console.log(element);//shows the modified element
+element.style.color='cyan'
+element.style.fontSize="40px"
+//element.style="display:none"
+console.log(element.classList);//lists all the classes
+element.classList.add('five')
+console.log(element.classList);
+element.classList.remove('one')
+console.log(element.classList);
+const body=document.querySelector('body')
+console.log(body.classList.toggle('dark'));//body has the dark class removes doesnt have adds class like if else .
+const value=element.classList.contains('three')//contains checks class exist or not in boolean
+console.log(value)

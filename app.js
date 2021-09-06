a4ca89ef5bcecb7e78038cd2330ecf39ee0a6353
Harini-Thandoju <harinithandoju@gmail.com>
2021-09-06 20:03:09 +0530

    sample dark mode using event listener

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const element=document.querySelector('body')
 
 
 button.addEventListener('click', () =>{
-    element.classList.toggle("dark")//if it has the aqua calss toggle removes if it doesnt adds it.toggle is a method 
+    element.classList.toggle("dark")//if it has the dark class , toggle removes it, if it doesnt , toggle adds the dark class
+    // toggle is a method 
    
 })

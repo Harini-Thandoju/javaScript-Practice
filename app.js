91f099f97dbd5c964612ef46140c8c41d7c37e8c
Harini-Thandoju <harinithandoju@gmail.com>
2021-09-07 18:07:33 +0530

    array methods

--- app.js
+++ app.js
@@ -13,3 +13,26 @@ while(Iteration<10){
     console.log(Iteration)
     Iteration++
 }
+
+const balls = document.getElementsByClassName('.ball')
+const body=document.querySelector('body')
+const arr=[1,2,3,4,5]
+const added=arr.push(6)
+console.log(arr.includes(5));//if ele presents return true or false
+console.log(arr.indexOf(1))//return index of specific ele
+console.log(arr.unshift(6));//adds the arr at the beggining of arr
+console.log(arr.shift())//remove the frst ele of arr
+console.log(arr.splice(2,2))//it will remove from 2ndindex 2 ele 
+console.log(added)
+
+console.log(arr)
+while(arr.length<15){
+    arr.push("Iteration")
+}
+console.log(arr.length)
+ 
+//for each
+arr.forEach((element,iteration)=>{//expects to write func iterates to every ele and prints
+    console.log(element,iteration)
+    
+})
